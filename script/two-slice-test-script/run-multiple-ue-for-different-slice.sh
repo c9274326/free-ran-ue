a64b74e36775748f268476c4aa8dee73b953fe9b
Zach1113 <zach911113@gmail.com>
2025-12-03 05:34:42 +0000

    refactor: place project test script in new folder

--- script/two-slice-test-script/run-multiple-ue-for-different-slice.sh
+++ script/two-slice-test-script/run-multiple-ue-for-different-slice.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 ########################################################
-# Script for running mixed UE traffic
+# Script for running multiple UE for different slices
 #
 # Usage:
-#   ./run-mixed-traffic.sh
+#   ./run-multiple-ue-for-different-slice.sh
 #
 # Description:
 #   This script runs two instances of free-ran-ue with
-#   different configurations to simulate mixed traffic.
+#   different configurations to simulate multiple UE for different slices.
 ########################################################
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
@@ -20,11 +20,14 @@ cd "$ROOT_DIR"
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
 echo "Starting UE for Slice 1..."
-./build/free-ran-ue ue -c config/ue1.yaml -n 90 -p 9 &
+./build/free-ran-ue ue -c config/ue1.yaml -n 90 &
 PID1=$!
 
+# echo "Waiting for Slice 1 UEs to initialize tunnel devices..."
+# sleep 30
+
 echo "Starting UE for Slice 2..."
-./build/free-ran-ue ue -c config/ue2.yaml -n 10 -p 1 &
+./build/free-ran-ue ue -c config/ue2.yaml -n 10 &
 PID2=$!
 
 echo "UEs started. Press Ctrl+C to stop."
